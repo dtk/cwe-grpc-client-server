3335a48eb739a093fb2e67b30a3d99e3117f1ae6
Vedad Fejzagic <vedad.fejzagic@atlantbh.com>
2019-04-22 13:32:11 +0200

    Add ping

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var packageDefinition = protoLoader.loadSync(
      oneofs: true
     });
 var executable_action_proto = grpc.loadPackageDefinition(packageDefinition).pckg_executable_action;
+const delay = 2000;
 
 function sleep(ms) {
   console.log("Waiting", ms/1000, "s")
@@ -28,6 +29,11 @@ function reqReceived(call, callback) {
     message: 'Server received the message'
   }
   callback(null, reqAck);
+}
+
+async function ping(call, callback) {
+  console.log("Ping received");
+  callback(null, { taskId: call.request.taskId, message: "Pong"})
 }
 
 async function streamAction(call, callback) {
@@ -55,36 +61,18 @@ async function streamAction(call, callback) {
     }, (err) => { console.log(err); });
   }
   else{
-    await sleep(2000);
+    await sleep(delay);
     callback(null, { taskId: call.request.taskId, message: 'Execution success' });
   }
   
 }
-
-// function streamAction(call) {
-//   call.on('data', async function (message) {
-//     console.log('Server: Stream Message Received = ', message); // Server: Stream Message Received = {id: 1}
-//     setTimeout(function () {
-//       call.write({
-//         task_id: message.taskId,
-//         id: message.id,
-//         message: "[SUCCESS]"
-//       })
-//     }, 2000)
-//   });
-
-//   call.on('end', function() {
-//     call.end();
-//   })
-// }
-
 /**
  * Starts an RPC server that receives requests for the Greeter service at the
  * sample server port
  */
 function main() {
   var server = new grpc.Server();
-  server.addService(executable_action_proto.ActionRequest.service, { reqReceived: reqReceived, streamAction: streamAction });
+  server.addService(executable_action_proto.ActionRequest.service, { reqReceived: reqReceived, streamAction: streamAction, ping: ping});
   server.bind('0.0.0.0:' + port, grpc.ServerCredentials.createInsecure());
   server.start();
   console.log("server started: ", server);
