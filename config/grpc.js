280f692112b51a8f91ad8a0074bdc61eef93c9d9
Vedad Fejzagic <vedad.fejzagic@atlantbh.com>
2019-05-31 10:20:00 +0200

    DTK-3789 add config, add test file for port busy stub

---
+++ config/grpc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  grpc: {
+    clientAddress: "0.0.0.0",
+    clientPort: "8083",
+    serverAddress: "0.0.0.0",
+    serverPort: "8082"
+  }
+};
--- send-action.js
+++ send-action.js
@@ -1,30 +1,31 @@
-var PROTO_PATH = 'received_message.proto';
-var received_message = require('./received_message');
-var grpc = require('grpc');
-var protoLoader = require('@grpc/proto-loader')
-var grpc_promise = require('grpc-promise');
+var PROTO_PATH = "received_message.proto";
+var received_message = require("./received_message");
+var grpc = require("grpc");
+var protoLoader = require("@grpc/proto-loader");
+var grpc_promise = require("grpc-promise");
+const envConfig = require("./config/grpc");
 
-var packageDefinition = protoLoader.loadSync(
-  PROTO_PATH,
-  {
-    keepCase: false,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true
-  }
-)
-var received_message_proto = grpc.loadPackageDefinition(packageDefinition).pckg_received_message;
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: false,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+});
+var received_message_proto = grpc.loadPackageDefinition(packageDefinition)
+  .pckg_received_message;
 
 async function main() {
-  var client = new received_message_proto.ActionRequest('0.0.0.0:8083', grpc.credentials.createInsecure());
+  var client = new received_message_proto.ActionRequest(
+    envConfig.grpc.clientAddress + ":" + envConfig.grpc.clientPort,
+    grpc.credentials.createInsecure()
+  );
   grpc_promise.promisifyAll(client, { timeout_message: 6000 });
   const send = client.sendAction();
   console.log("Sending: ", received_message);
-  try{
+  try {
     const res = await send.sendMessage(received_message);
-  }
-  catch(err) {
+  } catch (err) {
     console.log(err);
   }
 }
