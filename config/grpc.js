7e06a52e8680165e96d7070e707ec24cb7adb043
Vedad Fejzagic <vedad.fejzagic@atlantbh.com>
2019-06-27 12:48:17 +0200

    DTK-3799 edit proto files

--- config/grpc.js
+++ config/grpc.js
@@ -1,4 +1,10 @@
 module.exports = {
+  // grpc: {
+  //   clientAddress: "10.105.120.74",
+  //   clientPort: "3000",
+  //   serverAddress: "0.0.0.0",
+  //   serverPort: "8082"
+  // }
   grpc: {
     clientAddress: "0.0.0.0",
     clientPort: "8083",
--- received_message.js
+++ received_message.js
@@ -1,35 +1,3 @@
-var executable_actions = require("./executable_actions");
-
-let executable_actions_buff = Buffer.from(JSON.stringify(executable_actions));
-
-var decomposition = {
-  temporalRelationship: "SEQUENTIAL",
-  nestedActions: executable_actions_buff
-};
-
-var abstract_action = {
-  taskId: 0,
-  name: "MOD::COMP[TITLE].METHOD",
-  type: "ABSTRACT",
-  description: "Encodes a file and deploys to CDN",
-  retryCount: 1,
-  retryDelaySeconds: 1,
-  decomposition: decomposition,
-  schemaVersion: 2,
-  topTaskId: -1
-};
-
-// module.exports = received_message = {
-//   type: 'ABSTRACT',
-//   abstractAction: abstract_action
-// };
-
-// module.exports = received_message = {
-//   endpoint: "dtk.io",
-//   path: "v1alpha1.actions",
-//   index: 0
-// };
-
 cursor = {
   index: "-1",
   position: "after"
@@ -40,3 +8,5 @@ module.exports = received_message = {
   namespace: "test",
   cursor
 };
+
+// ./grpcurl -plaintext -proto received_message.proto -d '{"name": "create-component-group-test1","namespace": "test","cursor": {"index": "-1","position": "after"}}'
