0147a290d49de87b3d110787ae60babddd98c418
fvedad <vedad.fejzagic@atlantbh.com>
2019-08-21 10:57:16 +0000

    DTK-3844 uncomment debug

--- received_message.js
+++ received_message.js
@@ -17,7 +17,7 @@ module.exports = received_message = {
   name: "create-subnet-security-group",
   namespace: "default",
   cursor,
-// debug
+  debug
 };
 
 // ./grpcurl -plaintext -proto received_message.proto -d '{"name": "create-component-group-test1","namespace": "test","cursor": {"index": "-1","position": "after"}}'
